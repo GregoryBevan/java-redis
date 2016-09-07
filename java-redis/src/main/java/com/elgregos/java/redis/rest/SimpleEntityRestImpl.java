b55ed77f071fa180da91ce1159f7814c8e1df4ed
gbe <gbe@vif.fr>
2016-09-07 15:45:52 +0200

    Add rest service

--- java-redis/src/main/java/com/elgregos/java/redis/rest/SimpleEntityRestImpl.java
+++ java-redis/src/main/java/com/elgregos/java/redis/rest/SimpleEntityRestImpl.java
@@ -1,16 +1,25 @@
 package com.elgregos.java.redis.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.elgregos.java.redis.entities.SimpleEntity;
 import com.elgregos.java.redis.service.SimpleEntityService;
 
-@RestController("simple-entities")
+@RestController
+@RequestMapping("simple-entities")
 public class SimpleEntityRestImpl {
 
 	@Autowired
 	private SimpleEntityService service;
 
-	// public Respons
+	@RequestMapping(method = RequestMethod.GET)
+	public List<SimpleEntity> getAll() {
+		return service.getSimpleEntities();
+	}
 
 }
